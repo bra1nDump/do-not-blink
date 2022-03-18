e4f91a0b58f018841c87b383b7129dda38c868e9
Kirill Dubovitskiy <kirill2003de@gmail.com>
2022-03-18 00:02:22 -0700

    Join room and display state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,32 @@
-import React, { useRef, useState } from "react";
-import logo from "./logo.svg";
-import * as c from "colyseus.js";
+import React, { useEffect, useRef, useState } from "react";
+import { Client, Room } from "colyseus.js";
 import "./App.css";
 
+interface State {
+  mySynchronizedProperty: string;
+}
+
 function App() {
-  const client = useRef(new c.Client("ws://localhost:2567"));
-  async function createRoom() {
-    const room = await client.current.create("my_room", {});
-    room.send("play card");
-  }
+  const [room, setRoom] = useState<State | null>(null);
+  const client = useRef(new Client("ws://localhost:2567"));
+  useEffect(() => {}, [room]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button aria-label="Great room" onClick={createRoom} />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {room ? (
+          <div>{room.mySynchronizedProperty}</div>
+        ) : (
+          <button
+            onClick={async () => {
+              const room = await client.current.create<State>("my_room", {
+                id: "world",
+              });
+              room.onStateChange(setRoom);
+            }}
+          >
+            Create room
+          </button>
+        )}
       </header>
     </div>
   );
